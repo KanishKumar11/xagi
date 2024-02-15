75562e3957cd7b3b1b08fec7ad8d6565917e2c22
KanishKumar11 <kanishkumar11@outlook.in>
2024-02-15 20:00:33 +0530

    fixe

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,32 @@
-import {type NextRequest, NextResponse} from 'next/server'
-import {createClient} from '@/utils/supabase/middleware.ts'
+import { type NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-    try {
-        // This `try/catch` block is only here for the interactive tutorial.
-        // Feel free to remove once you have Supabase connected.
-        const {supabase, response} = createClient(request)
+  try {
+    // This `try/catch` block is only here for the interactive tutorial.
+    // Feel free to remove once you have Supabase connected.
+    const { supabase, response } = createClient(request);
 
-        // Refresh session if expired - required for Server Components
-        // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-        const {data: {session}} = await supabase.auth.getSession()
-        //
-        if (!session && request.nextUrl.pathname.startsWith('/dashboard')) {
-            // redirect to /login
-            return NextResponse.redirect(new URL('/login', request.url))
-        }
-
-        return response
-    } catch (e) {
-        // If you are here, a Supabase client could not be created!
-        // This is likely because you have not set up environment variables.
-        // Check out http://localhost:3000 for Next Steps.
-        return NextResponse.next({
-            request: {
-                headers: request.headers,
-            },
-        })
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    //
+    if (!session && request.nextUrl.pathname.startsWith("/dashboard")) {
+      // redirect to /login
+      return NextResponse.redirect(new URL("/login", request.url));
     }
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    // Check out http://localhost:3000 for Next Steps.
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+  }
 }
